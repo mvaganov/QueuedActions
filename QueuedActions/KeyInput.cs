d1b4cfa4b6ef0f6eaf7cf434b4b8bed2b6505c9f
Michael Vaganov <mvaganov@hotmail.com>
2025-06-12 11:11:15 -0500

    init commit

---
+++ QueuedActions/KeyInput.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QueuedActions {
+	public class KeyInput {
+		private static KeyInput instance;
+		List<ConsoleKeyInfo> keys = new List<ConsoleKeyInfo>();
+		public int Count => keys.Count;
+		public ConsoleKeyInfo this[int index] => keys[index];
+		public KeyInput Instance => instance != null ? instance : instance = new KeyInput();
+		public Dictionary<char, List<Action>> keyBinding = new Dictionary<char, List<Action>>();
+		private List<List<Action>> toExecuteThisFrame = new List<List<Action>>();
+		public KeyInput() {
+			if (instance == null) {
+				instance = this;
+			}
+		}
+		public void Update() {
+			keys.Clear();
+			while (Console.KeyAvailable) {
+				ConsoleKeyInfo key = Console.ReadKey();
+				keys.Add(key);
+				if (keyBinding.TryGetValue(key.KeyChar, out List<Action> actions)) {
+					toExecuteThisFrame.Add(actions);
+				}
+			}
+			toExecuteThisFrame.ForEach(actions => actions.ForEach(a => a.Invoke()));
+			toExecuteThisFrame.Clear();
+		}
+		public bool HasKey(char keyChar) => GetKeyIndex(keyChar) != -1;
+		public int GetKeyIndex(char keyChar) {
+			for (int i = 0; i < keys.Count; i++) {
+				if (keys[i].KeyChar == keyChar) { return i; }
+			}
+			return -1;
+		}
+		public List<Action> BindKey(char key, Action action) {
+			if (!keyBinding.TryGetValue(key, out List<Action> actions)) {
+				actions = new List<Action>();
+				keyBinding[key] = actions;
+			}
+			actions.Add(action);
+			return actions;
+		}
+		/// <param name="key"></param>
+		/// <param name="action">if null, removes all actions bound to this key</param>
+		/// <returns></returns>
+		public int Unbind(char key, Action action) {
+			int removedCount = 0;
+			if (!keyBinding.TryGetValue(key, out List<Action> actions)) {
+				return removedCount;
+			}
+			for (int i = actions.Count - 1; i >= 0; --i) {
+				if (action != null && actions[i] != action) {
+					continue;
+				}
+				actions.RemoveAt(i);
+				++removedCount;
+			}
+			return removedCount;
+		}
+	}
+}
---
+++ QueuedActions/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace QueuedActions {
+	public class Program {
+		public static void Print(string message, int row = -1, int col = 0) {
+			if (row >= 0 && col >= 0) {
+				Console.SetCursorPosition(col, row);
+			}
+			Console.Write(message);
+		}
+		static void Main(string[] args) {
+			ActionManager q = new ActionManager();
+			KeyInput keyInput = new KeyInput();
+			bool running = true;
+			long now = Environment.TickCount;
+			long then, soon;
+			long deadmanSwitchStarted = Environment.TickCount;
+
+			bool HasKeyPress() {
+				if (keyInput.Count > 0) { Print("Got em!", 20); return true; }
+				return false;
+			}
+			bool ProgramTimeout() => Environment.TickCount > deadmanSwitchStarted + 5000;
+			bool EscapePressCheck() {
+				Print("waiting for esc..." + Environment.TickCount, 21);
+				if (keyInput.HasKey((char)27)) {
+					Print("DONE                        ", 22);
+					running = false;
+					return true;
+				}
+				return false;
+			}
+			void RestartThisAction() {
+				q.ResetAllActions();
+			}
+
+			q.Add(new ActionEntry {
+				ID = "Quit On Escape",
+				WhatToDo = () => { Print("\npress escape to quit", 22); },
+				States = new ActionState[] { new ActionState("escapeKey", EscapePressCheck), }, LayerMask = 1
+			});
+			q.Add(new ActionEntry {
+				ID = "wait", WaitTime = 3,
+				WhatToDo = () => { Console.WriteLine("\nwaiting             "); }
+			});
+			q.Add(new ActionEntry {
+				ID = "Wait for key press", WaitTime = 0.5f,
+				WhatToDo = () => {
+					Console.WriteLine("\npress key to continue (you have 5 seconds)");
+					deadmanSwitchStarted = Environment.TickCount;
+				},
+				States = new ActionState[]{
+					new ActionState("timeout", ProgramTimeout, () => running = false),
+					new ActionState("success", HasKeyPress),
+				}
+			});
+			bool paused = false;
+			keyInput.BindKey(' ', () => paused = !paused);
+			keyInput.BindKey('\b', RestartThisAction);
+			while (running) {
+				keyInput.Update();
+				then = now;
+				now = Environment.TickCount;
+				float deltaTime = (now - then) / 1000f;
+				if (!paused) {
+					q.Update(deltaTime);
+				}
+				Console.SetCursorPosition(0, 0);
+				q.PrintStuff();
+				Console.WriteLine($"{q.WaitTime:0.00} {deltaTime:0.00}          \n");
+				soon = now + 10;
+				while (Environment.TickCount < soon && keyInput.Count == 0) {
+					System.Threading.Thread.Sleep(1);
+				}
+			}
+			Console.WriteLine("finished");
+		}
+	}
+}
